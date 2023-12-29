bafa1a03ccbe335f9e00d120275f8ea8fb40ded0
sayankbanik <39166919+sayankbanik@users.noreply.github.com>
2023-12-29 11:08:33 +0530

    demo.java file added to each weeek modules

---
+++ CN_Java/week 3/demo.java
@@ -0,0 +1,7 @@
+//package CN_Java.week 3;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+    }
+}
---
+++ CN_Java/week 4/demo.java
@@ -0,0 +1,7 @@
+//package CN_Java.week 4;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
